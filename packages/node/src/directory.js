9baaa20325814c20705643f90e96ea5fcaccdb3a
Vu Anh Hao <hi@haova.me>
2023-07-26 15:40:37 +0700

    update directory

--- packages/node/src/directory.js
+++ packages/node/src/directory.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
-import { process } from 'node:process';
+import process from 'node:process';
+import { serveDirectory } from './index.js';
 
 serveDirectory(process.env.PORT);
